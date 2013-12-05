3317602d456a825728aa1f30e957da073419df38
Brandon Ewoldt <bewoldt1@gmail.com>
2013-12-05 12:33:25 -0600

    updating wild rose book flyer to add featured images underneath main image. Also made it responsive.
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- mysite/code/EFlyer.php
+++ mysite/code/EFlyer.php
@@ -8,6 +8,8 @@ class EFlyer extends Page {
 	
 	'FooterText' => 'HTMLText',
 	'ImageURL' => 'Text',
+	'Feature1URL' => 'Text',
+	'Feature2URL' => 'Text',
 					  
    );
    
@@ -16,6 +18,8 @@ class EFlyer extends Page {
    private static $has_one = array(
 						   
 		'MainImage' => 'Image',
+		'Feature1Image' => 'Image',
+		'Feature2Image' => 'Image',
 	  
 	  );
 
@@ -26,10 +30,18 @@ function getCMSFields() {
 	/* Main Content Fields */
 	$fields->removeFieldFromTab("Root.Main","Content");
 	//Remove the content field so we can re-add and shrink the number of rows it uses
-	$fields->addFieldToTab('Root.Main', new HTMLEditorField('Content', 'Main Text', 5	));
-		$fields->addFieldToTab('Root.Main', new HTMLEditorField('FooterText', 'Footer Text', 5	));	
+	$fields->addFieldToTab('Root.Main', new HTMLEditorField('Content', 'Main Text', 5	));	
 	$fields->addFieldToTab('Root.Main', new TextField('ImageURL', 'Image Link URL (optional)'));
 	$fields->addFieldToTab('Root.Main', new UploadField('MainImage', 'Main Image - 500 x 636 pixels', null, null, null, "eflyer_images"));
+	// $fields->addFieldToTab('Root.Main', new HTMLEditorField('FooterText', 'Footer Text', 5	));
+
+	/* Feature 1 content */
+	$fields->addFieldToTab('Root.Feature1', new UploadField('Feature1Image', 'Upload Image'));
+	$fields->addFieldToTab('Root.Feature1', new TextField('Feature1URL', 'Image Link URL (optional)'));
+
+	/* Feature 2 content */
+	$fields->addFieldToTab('Root.Feature2', new UploadField('Feature2Image', 'Upload Image'));
+	$fields->addFieldToTab('Root.Feature2', new TextField('Feature2URL', 'Image Link URL (optional)'));
 
 	return $fields;
 	
