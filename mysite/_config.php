4dd3a9804611a7e6588450903b257690a450f403
javeriaaali <javeria-ali@uiowa.edu>
2015-08-12 13:33:32 -0500

    Update _config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -22,4 +22,10 @@ HtmlEditorConfig::get('cms')->setOption('paste_remove_styles','true');
 HtmlEditorConfig::get('cms')->setOption('paste_remove_styles_if_webkit','true');
 HtmlEditorConfig::get('cms')->setOption('paste_strip_class_attributes','true');
 
+
+if(Director::isLive()) {
+	Director::forceSSL(array('/^Security/','/^admin/'));
+}
+
 ?>
+
